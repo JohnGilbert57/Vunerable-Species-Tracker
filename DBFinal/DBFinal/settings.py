d67c90db4ed15440ef3dcbe139b042d736acaebb
John Gilbert <jg480318@ohio.edu>
2021-11-03 17:05:45 -0400

    Issue-1: UI set up and tables ares ready to be filled in

--- DBFinal/DBFinal/settings.py
+++ DBFinal/DBFinal/settings.py
@@ -36,7 +36,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DBFinal.settings")
 # Application definition
 
 INSTALLED_APPS = [
-    #'clearcache',
+    'clearcache',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
